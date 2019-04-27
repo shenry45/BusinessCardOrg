29119c4694c7167ca4d0d8ff5ccb84df270465dd
shenry45 <shawn45henry@gmail.com>
2019-04-26 20:54:20 -0700

    add media queries for nav and hero

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,11 @@
 // menu controller
 const mobileNav = document.querySelector('.mobile a.nav');
-
-mobileNav.addEventListener('click', () => {
+const mobileLinks = document.querySelectorAll('.mobile nav a');
+const hideMenu = () => {
   const mobileMenu = document.querySelector('.mobile nav');
 
   mobileMenu.classList.toggle('active');
-});
+}
+
+mobileLinks.forEach(el => el.addEventListener('click', hideMenu));
+mobileNav.addEventListener('click', hideMenu);
