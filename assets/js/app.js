db19cb934b04b048183aaded23f594d93716226e
shenry45 <shawn45henry@gmail.com>
2019-05-04 22:29:35 -0700

    accordion dropdown, incomplete

--- assets/js/app.js
+++ assets/js/app.js
@@ -77,16 +77,62 @@ if (document.querySelector('.testim') != null || document.URL.includes('index.ht
 
 // faq accordion
 if (document.querySelector('.faq') != null || document.URL.includes('pricing.html')) {
+
+  // create class for buttons and click
   class Button {
     constructor(el) {
       this.el = el;
-      this.el.addEventListener('click', () => { this.expandCont() });
+      this.el.addEventListener('click', (e) => { this.expandCont(e) });
     }
     expandCont(e) {
-      console.log(e);
+      // get parent of clicked arrow icon
+      const currAccItem = e.target.parentNode;
+
+      if (e.target.classList.value.includes('flipUp')) {
+        // remove all current el content
+        resetAccCont();
+
+        this.el.classList.remove('flipUp');
+        this.el.classList.add('flipDown');
+      } 
+      else {
+        // remove all current el content
+        resetAccCont();
+        // remove flipUp class from all icons
+        resetIcons();
+
+        // get data el from the content
+        const currAccCont = document.querySelector(`.acc-content div[data-cont="${currAccItem.dataset.acc}"] p`);
+        
+        // insert HTML content matching target dataset
+        currAccItem.insertAdjacentHTML('beforeend', `<p class="acc-info">${currAccCont.innerHTML}</p>`);
+        
+        // reset icon rotate and set clicked icon to point up
+        resetRotates();
+        this.el.classList.add('flipUp');
+      }
+
+      
+    }
+  }
+  
+  const faqs = document.querySelectorAll('.faq .acc p.icon');
+  faqs.forEach(el => new Button(el));
+
+  const resetRotates = () => {
+    faqs.forEach(el => el.style.transform = '');
+  }
+
+  const resetAccCont = () => {
+    document.querySelectorAll('p.acc-info').forEach(el => el.parentNode.removeChild(el));
+  }
+
+  const resetIcons = () => {
+    const getIcons = document.querySelectorAll('p.icon.flipDown');
+
+    if (getIcons.length > 0) {
+      getIcons.forEach(el => el.classList.remove('flipDown'));
     }
   }
 
-  const faqs = document.querySelectorAll('.faq .acc p.icon').forEach(el => new Button(el));
-  
 }
